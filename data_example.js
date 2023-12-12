1a77fa2d7e8edf55f18324fa69ba631112a3263c
NamaWorks <pedronayamartinez@hotmail.com>
2023-12-12 20:01:28 +0100

    add first commit

---
+++ data_example.js
@@ -0,0 +1,80 @@
+const data = {
+  name: "Peter Parker",
+  address: "123 Main Street, New York, NY",
+  email: "peter.parker@email.com",
+  avatar:
+    "https://i.pinimg.com/736x/8f/c2/ad/8fc2adfb17b261162bbd9a4af5239c64.jpg",
+  aboutMe:
+    "I'm a passionate photographer, web developer, and journalist with a diverse educational background. I hold a Bachelor's degree in Science, a Web Development Certification, and a Master's in Journalism. My work experience includes capturing exclusive images as a photographer, developing web applications, and reporting on news stories. I'm committed to continuous learning and contributing to both the digital world and quality journalism.",
+
+  education: {
+    degree: "Bachelor of Science in Computer Science",
+    university: "City University of New York",
+    graduationYear: 2012,
+    honors: "Summa Cum Laude",
+    relevantCourses: [
+      "Web Development Fundamentals",
+      "Data Structures and Algorithms",
+      "Database Design",
+      "User Interface Design",
+    ],
+  },
+  workExperience: [
+    {
+      position: "Web Developer",
+      company: "Tech Solutions Inc.",
+      startDate: "2013",
+      endDate: "Present",
+      description:
+        "As a key member of the development team, I have contributed to various projects, from creating interactive web applications to optimizing database performance.",
+    },
+    {
+      position: "Freelance Web Developer",
+      company: "Self-employed",
+      startDate: "2012",
+      endDate: "2013",
+      description:
+        "During my freelancing period, I collaborated with clients from diverse industries, delivering tailor-made websites that met their unique needs and goals.",
+    },
+  ],
+  skills: [
+    "HTML5",
+    "CSS3",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Express.js",
+    "MongoDB",
+    "RESTful API",
+    "Git",
+    "Frontend and Backend Development",
+  ],
+  projects: [
+    {
+      title: "E-commerce Website",
+      description:
+        "Designed and developed a feature-rich e-commerce platform with advanced product search, user reviews, and a secure payment system, resulting in a 30% increase in sales for the client.",
+      link: "http://github.com/#",
+      preview:
+        "https://plus.unsplash.com/premium_photo-1684785618727-378a3a5e91c5?auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZWNvbW1lcmNlfGVufDB8fDB8fHww&w=500",
+    },
+    {
+      title: "Blog Platform",
+      description:
+        "Created a customizable blog platform with dynamic content creation, user authentication, comment functionality, and social media integration, leading to a 50% growth in user engagement.",
+      link: "http://github.com/#",
+      preview:
+        "https://plus.unsplash.com/premium_photo-1684581214880-2043e5bc8b8b?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2070",
+    },
+    {
+      title: "Portfolio Website",
+      description:
+        "Built a professional portfolio website to showcase my web development skills and projects. The site features a clean, responsive design and highlights my work as a developer.",
+      link: "http://github.com/#",
+      preview:
+        "https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2071",
+    },
+  ],
+};
+
+export default data;
